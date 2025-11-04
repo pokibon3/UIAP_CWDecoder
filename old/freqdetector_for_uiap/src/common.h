f25228bc0691153e1830725be0e1820722f404b0
ooe <ooe@k.email.ne.jp>
2025-11-05 00:05:45 +0900

    bug fix

---
+++ old/freqdetector_for_uiap/src/common.h
@@ -0,0 +1,33 @@
+#define GPIO_ADC_MUX_DELAY 100
+#define GPIO_ADC_sampletime GPIO_ADC_sampletime_43cy
+
+#define SCALE 3
+
+#define micros() (SysTick->CNT / DELAY_US_TIME)
+#define millis() (SysTick->CNT / DELAY_MS_TIME)
+
+//#define LED_PIN GPIOv_from_PORT_PIN(GPIO_port_D, 5)
+#define SW1_PIN GPIOv_from_PORT_PIN(GPIO_port_A, 1)		// for uiap
+#define SW2_PIN GPIOv_from_PORT_PIN(GPIO_port_C, 4)		// for uiap
+#define SW3_PIN GPIOv_from_PORT_PIN(GPIO_port_D, 2)
+#define ADC_PIN GPIOv_from_PORT_PIN(GPIO_port_A, 2)		// for uiap
+#define LED_PIN GPIOv_from_PORT_PIN(GPIO_port_C, 0)		// for uiap
+#define UART_PIN GPIOv_from_PORT_PIN(GPIO_port_D, 5)
+#define TEST_PIN GPIOv_from_PORT_PIN(GPIO_port_D, 6)
+
+#define TEST_HIGH			GPIO_digitalWrite(TEST_PIN, high);
+#define TEST_LOW			GPIO_digitalWrite(TEST_PIN, low);
+
+#define SAMPLES 128
+#define BUFSIZE (SAMPLES * 2)
+#define SAMPLING_FREQUENCY 6000	// Hz	+10%
+
+extern uint16_t sampling_period_us;
+//extern int8_t vReal[SAMPLES];
+//extern int8_t vImag[SAMPLES];
+
+// function prototype (declaration), definition in "ch32v003fun.c"
+extern "C" int mini_snprintf(char* buffer, unsigned int buffer_len, const char *fmt, ...);
+
+extern int GPIO_setup();
+extern int check_input();
